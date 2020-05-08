7b99b0404d036351115404e3b79312bee9ce67ad
Sujit Kumar <60378235+therealsujitk@users.noreply.github.com>
2020-05-08 21:30:55 +0530

    Initial commit

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("localhost","root","299792458","mafia");
+	$conn = mysqli_connect("localhost","root","","mafia");
 	if (!$conn) {
 		die("Sorry, there was an error connecting to our database. Please contact <b>BinaryStack</b> if this error persists.");
 	}
