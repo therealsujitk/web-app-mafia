b5d157620be087643a02eff8dc0ddfa3fe460dde
Sujit Kumar <kumarsujit2002@gmail.com>
2020-07-22 19:43:32 +0530

    Minor improvements

--- stats.php
+++ stats.php
@@ -14,7 +14,7 @@
 		<meta name="msapplication-TileColor" content="#ffffff">
 		<meta name="theme-color" content="#c80000">
 		<meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="user-scalable=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<title>Statistics - Mafia</title>
 	</head>
 	<body>
